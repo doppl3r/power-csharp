4e7d52b27aa674e9626707d581eacaa98e9cb635
Jacob DeBenedetto <deben3@gmail.com>
2014-05-05 17:21:38 -0700

    first commit

---
+++ power-csharp/Power.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_csharp
+{
+    class Power
+    {
+        public Power()
+        {
+
+        }
+        public void calculate(double[,] a, double[,] x, double tol, int n){
+        //declare and initialize variables
+        int k = 1;
+        double yp;
+        double err;
+        double mu = 0;
+        double[,] y;
+        //mutate vars
+        x = divideMatrix(x,getHighestValue(x));
+
+        //loop through
+        while (k <= n){
+            y  = multiplyMatrix(a, x);
+            mu = getHighestValue(y);
+            yp = getHighestValue(y);
+            if (yp == 0){
+                Console.WriteLine("A");
+                Console.WriteLine("A has a zero eigenvalue with corresponding e_vect");
+                break;
+            }
+            err = norm(subtractMatrix(x, divideMatrix(y, getHighestValue(y))));
+            x = divideMatrix(y, getHighestValue(y));
+            if (err < tol){
+                Console.WriteLine("err: " + err + ", tol: " + tol);
+                break;
+            }
+            k++;
+        }
+        Console.WriteLine("Coverages to mu=" + mu);
+    }
+        public double getHighestValue(double[,] matrix) { return matrix[getHighestValueIndex(matrix),0]; }
+        public int getHighestValueIndex(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int index = 0;
+            double p = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (p < matrix[row,col])
+                    {
+                        p = matrix[row,col];
+                        index = row;
+                    }
+                }
+            }
+            return index;
+        }
+        public double[,] divideMatrix(double[,] matrix, double denominator)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row,col] = matrix[row,col] / denominator;
+                }
+            }
+            return matrix;
+        }
+        public double norm(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            double total = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    total += Math.Pow(matrix[row,col], 2);
+                }
+            }
+            return Math.Sqrt(total);
+        }
+        public double[,] subtractMatrix(double[,] A, double[,] B){
+            int aRows = A.GetLength(0);
+            int aCols = A.GetLength(1);
+            int bRows = B.GetLength(0);
+            int bCols = B.GetLength(1);
+
+            double[,] matrix = new double[aRows,bCols];
+            for (int row = 0; row < aRows; row++) {
+                for (int col = 0; col < bCols; col++) {
+                    matrix[row,col] = A[row,col]-B[row,col];
+                }
+            }
+            return matrix;
+        }
+        public double[,] multiplyMatrix(double[,] A, double[,] B) {
+            int aRows = A.GetLength(0);
+            int aCols = A.GetLength(1);
+            int bRows = B.GetLength(0);
+            int bCols = B.GetLength(1);
+
+            if (aCols != bRows) {
+                //throw new IllegalArgumentException("A:Rows: " + aCols + " did not match B:Columns " + bRows + ".");
+            }
+
+            double[,] matrix = new double[aRows,bCols];
+            for (int i = 0; i < aRows; i++) {
+                for (int j = 0; j < bCols; j++) {
+                    matrix[i,j] = 0;
+                }
+            }
+
+            for (int i = 0; i < aRows; i++) { // aRow
+                for (int j = 0; j < bCols; j++) { // bColumn
+                    for (int k = 0; k < aCols; k++) { // aColumn
+                        matrix[i,j] += A[i,k] * B[k,j];
+                    }
+                }
+            }
+
+            return matrix;
+        }
+        public void matrixToString(double[,] matrix){
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int row = 0; row < rows; row++){
+                for (int col = 0; col < cols; col++){
+                    Console.Write(matrix[row,col]);
+                    if (col != cols && row != rows) Console.Write(",");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ power-csharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_csharp
+{
+    class Program
+    {
+        //declare variables
+        static double[,] a; //matrix #1
+        static double[,] x; //matrix #2
+        static double tol; //tolerance z^x
+        static int n; //iterations
+
+        static void Main(string[] args)
+        {
+            //These stay the same per test
+            Power power = new Power();
+            x = new double[,]{ //row, column
+                {500},
+                {300},
+                {150},
+                { 50}
+            };
+            tol = Math.Pow(10, -5);
+            n = 100;
+
+            //Test1
+            a = new double[,]{
+                { 0, 0.75, 2.4, 0.3},
+                { 0.4, 0, 0, 0},
+                { 0, 0.7, 0, 0},
+                { 0, 0, 0.8, 0}
+            };
+            power.calculate(a, x, tol, n);
+
+            //Test2
+            a = new double[,]{
+                { 0, 0.75, 2.4, 0.3},
+                { 0.2, 0, 0, 0},
+                { 0, 0.1, 0, 0},
+                { 0, 0, 0.1, 0}
+            };
+            power.calculate(a, x, tol, n);
+
+            //Test3
+            a = new double[,]{
+                { 0, 1.2, 2.8, 0.7},
+                { 0.6, 0, 0, 0},
+                { 0, 0.9, 0, 0},
+                { 0, 0, 0.8, 0}
+            };
+            power.calculate(a, x, tol, n);
+
+            //Test4
+            a = new double[,]{
+                { 0, 1.8, 3, 1},
+                { 0.8, 0, 0, 0},
+                { 0, 0.95, 0, 0},
+                { 0, 0, 0.9, 0}
+            };
+            power.calculate(a, x, tol, n);
+
+            Console.ReadKey();
+        }
+    }
+}
